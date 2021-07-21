cbd021d4b1c174bbcb3e90b43dd737901d0ccf3e
Craig Satchell <satchdev@gmail.com>
2021-07-21 15:21:20 -0400

    refactor code

--- startup/db.js
+++ startup/db.js
@@ -1,17 +1,17 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
-
 function connectDB() {
-    mongoose.connect(
-        process.env.mongodbURI,
-        { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => console.log('Connected to MongoDB...'))
-        .catch((err) => {
-            console.log(`Could not connect to MongoDB. ERROR: ${err}`);
-            process.exit(1);
-        });
+   mongoose.connect(
+      process.env.mongodbURI,
+      { useNewUrlParser: true, useUnifiedTopology: true })
+      .then(() => console.log('Connected to MongoDB...'))
+      .catch((err) => {
+         console.log(`Could not connect to MongoDB. ERROR: ${err}`);
+         process.exit(1);
+      });
 }
 
 export default connectDB;
