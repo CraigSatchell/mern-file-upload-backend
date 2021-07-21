192eec09a07adc54df6b1fcd5d9e6187e09b6dec
Craig Satchell <satchdev@gmail.com>
2021-07-21 01:39:50 -0400

    reformat code

--- routes/items.js
+++ routes/items.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { getItems,createItem } from '../controllers/items.js';
 
 const router = express.Router();
+
 router.get('/',getItems)
 router.post('/', createItem);
 
